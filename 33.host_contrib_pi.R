f5e1dbd986a2553a2dcde7cf06221f2283796909
duaulthel <138584055+duaulthel@users.noreply.github.com>
2023-07-05 10:06:13 +0200

    33.host_contrib_pi.R

--- 33.host_contrib_pi.R
+++ 33.host_contrib_pi.R
@@ -12,9 +12,6 @@ library(lme4)
 
 #------------------------------------
 #------------------------------------
-setwd("C:/Thèse_ANSES/codes_R/Ttrees_ref")
-
-
 #import parameter results from every method
 samp <- "B1"
 
