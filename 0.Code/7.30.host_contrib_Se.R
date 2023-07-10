3d9de8ae320362ee6bb09e425c99467cda0df3d2
duaulthel <138584055+duaulthel@users.noreply.github.com>
2023-07-10 16:10:31 +0200

    7. Host contribution

--- 0.Code/7.30.host_contrib_Se.R
+++ 0.Code/7.30.host_contrib_Se.R
@@ -1,12 +1,13 @@
 rm(list=ls())
 #------------------------------------
 #packages
-library(tidyverse)
-library(broom)
-library(MASS)
+library(tidyverse) #version 1.3.0
+library(broom) #version 0.7.2
+library(MASS) #version 7.3-53
+
 #------------------------------------
 ##Description:
-##Alternative transmission scenarios
+##Alternative transmission scenarios: A1 (dead-end) and S4 (high mutation rate)
 ##Negative Binomial GLM on the host contribution indicator
 ##Comparison with induced subtree 
 
@@ -14,7 +15,7 @@ library(MASS)
 #------------------------------------
 #import host contribution results from every method
 out <- NULL
-for (i in c("A1", "S4")){
+for (i in c("A1", "S4")){ #A1 (dead-end) and S4 (high mutation rate)
   samp <- i
   
   out_o1 <- read_csv(paste0("./Host_contribution/outbreaker_",samp,"_reff.csv"))
@@ -37,8 +38,8 @@ out <- out %>% filter(scenario==1) %>% #only keep reference scheme
 
 
 #Correct names for transmission scenarios
-out$scen_sim <- case_when(out$scen_sim == "A1" ~ "CNTrW",
-                          out$scen_sim == "S4" ~ "CTrW_H")
+out$scen_sim <- case_when(out$scen_sim == "A1" ~ "CNTrW", #meaning cattle index and wild boars that did NOT transmit
+                          out$scen_sim == "S4" ~ "CTrW_H") #meaning cattle index, wild boars that transmit and high rate
 
 #Estimate sum
 out <- out %>% mutate(R=R*nb, R_sim=R_sim*nb_sim)
@@ -58,7 +59,7 @@ model <- glm.nb(R_sim
 
 summary(model) #and to get p-value
 
-#Create data.frame with results
+#Create data.frame with results (S12 Table)
 all_meth <- tidy(model, exponentiate = TRUE) %>%
   mutate(IRR=round(estimate, 2),
          p.value=ifelse(p.value<0.001, "<0.001", round(p.value,3)))
