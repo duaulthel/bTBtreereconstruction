63fe05c54c8dd6694f61814c9c8d322d70bc6308
duaulthel <138584055+duaulthel@users.noreply.github.com>
2023-07-10 16:30:51 +0200

    9. Index case

--- 0.Code/9.40.index_seqtrack.R
+++ 0.Code/9.40.index_seqtrack.R
@@ -2,7 +2,7 @@ rm(list=ls())
 
 #--------------------------------
 #packages
-library(tidyverse)
+library(tidyverse) #version 1.3.0
 
 #--------------------------------
 #Description:
@@ -12,12 +12,12 @@ library(tidyverse)
 #------------------------------------
 #------------------------------------
 #Transmission scenario considered
-samp <- "B1"
+samp <- "B1" #B1 (reference scenario) or B2 (badger index)
 
-nb_scheme <- ifelse(samp=="B1", 6, 1)
+nb_scheme <- ifelse(samp=="B1", 6, 1) #do not change B1, only scenario with 6 schemes
 #------------------------------------
 
-for (j in 1:30){#j is the tree number
+for (j in 1:30){ #j is the tree number
   
   sim <- paste0(samp,"_",j)
   
@@ -61,7 +61,8 @@ for (j in 1:30){#j is the tree number
     prev <- read_csv(paste0("seqTrack_",samp,"_index.csv"))
     index <- rbind(prev, index)
     write_csv(index , paste0("seqTrack_",samp,"_index.csv"))
-  }else{ #if this is the first tree, write new file
+  }
+  else{ #if this is the first tree, write new file
     write_csv(index , paste0("seqTrack_",samp,"_index.csv"))
   }
   
