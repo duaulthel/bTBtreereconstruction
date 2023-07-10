8b9a206636cce6a76c51f3a8ddfa49b83eb40f45
duaulthel <138584055+duaulthel@users.noreply.github.com>
2023-07-10 15:53:42 +0200

    6. Outbreak size

--- 0.Code/6.24.param_pi.R
+++ 0.Code/6.24.param_pi.R
@@ -1,10 +1,11 @@
 rm(list=ls())
 #------------------------------------
 #packages
-library(tidyverse)
-library(broom)
-library(MASS)
-library(lme4)
+library(tidyverse) #version 1.3.0
+library(broom) #version 0.7.2
+library(lme4) #version 1.1-29
+library(MASS) #version 7.3-53
+
 #------------------------------------
 ##Description:
 ##Negative Binomial GLMM on the outbreak size indicator
@@ -14,7 +15,7 @@ library(lme4)
 #------------------------------------
 #Estimate number of sequences per set of cases
 
-samp <- "B1"
+samp <- "B1" #B1 (reference scenario) 
 a <- substr(samp, 1, 1)
 for (j in 1:30){ #all trees in transmission scenarios
   for (i in 1:6){ #all sampling schemes
@@ -27,8 +28,6 @@ for (j in 1:30){ #all trees in transmission scenarios
 
 
 #import parameter results from every method
-
-samp <- "B1"
 
 out_o1 <- read_csv(paste0("./mcmc/outbreaker_",samp,"_mcmc.csv"))
 out_o1$method <- "outbreaker2"
@@ -58,19 +57,19 @@ out <- out %>% group_by(sim, method) %>%
          est_size= round(nb/median,0))
 
 #Correct names for sampling scenarios
-out$scenario <- case_when(out$scenario == 1 ~ "A",
+out$scenario <- case_when(out$scenario == 1 ~ "Reference",
                           out$scenario == 2 ~ "T",
                           out$scenario == 4 ~ "SB",
                           out$scenario == 3 ~ "SW",
                           out$scenario == 5 ~ "T+SW",
                           out$scenario == 6 ~ "T+SB")
-out$scenario <- factor(out$scenario, levels=c("A", "T", "SB", "T+SB", "SW", "T+SW"))
+out$scenario <- factor(out$scenario, levels=c("Reference", "T", "SB", "T+SB", "SW", "T+SW"))
 
 #scen_sim stands for transmission scenarios
 out <- out %>% mutate(scen_sim=substr(sim, 1, 2))
 
 #Correct names for transmission scenarios
-out$scen_sim <- "CTrW"
+out$scen_sim <- "CTrW" #meaning cattle index and wild boars that transmit
 
 #Write output for figure outbreak size
 write_csv(out, "res_outbreak_size_pi.csv")
@@ -100,5 +99,5 @@ ctab <- cbind(est=fixef(model2),cc)
 #Exponentiate results to get IRR
 rtab <- exp(ctab)
 
-#Create data.frame with results
+#Create data.frame with results (S2 Appendix)
 tab <- as.data.frame(round(rtab, 2))
