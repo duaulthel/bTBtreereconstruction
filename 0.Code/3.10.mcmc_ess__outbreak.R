f9ed4109a7e1bbdec82cc2d88bc279e44dce7b8e
duaulthel <138584055+duaulthel@users.noreply.github.com>
2023-07-10 15:10:29 +0200

    3. Convergence check

--- 0.Code/3.10.mcmc_ess__outbreak.R
+++ 0.Code/3.10.mcmc_ess__outbreak.R
@@ -1,8 +1,8 @@
 rm(list=ls())
 #------------------------------------
 #packages
-library(tidyverse)
-library(coda)
+library(tidyverse) #version 1.3.0
+library(coda) #version 0.19-4
 
 #------------------------------------
 ##Description:
@@ -14,12 +14,12 @@ library(coda)
 #------------------------------------
 
 #Transmission scenario considered
-samp <- "B1"
+samp <- "B1" #B1 (reference scenario) or A1 (dead-end), B2 (badger index), S1 (single-host), S4 (high mutation rate)
 
 #Number of sampling schemes
-nb_scheme <- ifelse(samp=="B1", 6, 1)
+nb_scheme <- ifelse(samp=="B1", 6, 1) #do not change B1, only scenario with 6 schemes
 
-for (j in 1:30){#all trees in transmission scenario
+for (j in 1:30){ #all trees in transmission scenario
   
   out_conv <- NULL
   sim <- paste0(samp,"_",j)
@@ -50,7 +50,8 @@ for (j in 1:30){#all trees in transmission scenario
     prev <- read_csv(paste0("outbreaker_",samp,"_mcmc.csv"))
     out_conv <- rbind(prev, out_conv)
     write_csv(out_conv, paste0("outbreaker_",samp,"_mcmc.csv"))
-  }else{ #if this is the first tree, write new file
+  }
+  else{ #if this is the first tree, write new file
     write_csv(out_conv, paste0("outbreaker_",samp,"_mcmc.csv"))
   }
 }
