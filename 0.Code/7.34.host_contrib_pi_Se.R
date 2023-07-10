f1e87a926ed0c71b22db1d140127e01aa23b25db
duaulthel <138584055+duaulthel@users.noreply.github.com>
2023-07-10 16:18:59 +0200

    7. Host contribution

--- 0.Code/7.34.host_contrib_pi_Se.R
+++ 0.Code/7.34.host_contrib_pi_Se.R
@@ -1,12 +1,13 @@
 rm(list=ls())
 #------------------------------------
 #packages
-library(tidyverse)
-library(broom)
-library(MASS)
+library(tidyverse) #version 1.3.0
+library(broom) #version 0.7.2
+library(MASS) #version 7.3-53
+
 #------------------------------------
 ##Description:
-##Alternative transmission scenarios
+##Alternative transmission scenarios: A1 (dead-end) and S4 (high mutation rate)
 ##Negative Binomial GLMM on the host contribution indicator
 ##Comparison with reference tree
 
@@ -14,7 +15,7 @@ library(MASS)
 #------------------------------------
 #import parameter results from every method
 out <- NULL
-for (i in c("A1", "S4")){
+for (i in c("A1", "S4")){ #A1 (dead-end) and S4 (high mutation rate)
   samp <- i
   
   out_o1 <- read_csv(paste0("./mcmc/outbreaker_",samp,"_mcmc.csv"))
@@ -38,7 +39,7 @@ pi_param <- out %>%
 
 #import host contribution results from every method
 out <- NULL
-for (i in c("A1", "S4")){
+for (i in c("A1", "S4")){ #A1 (dead-end) and S4 (high mutation rate)
   samp <- i
   
   out_o1 <- read_csv(paste0("./Host_contribution/outbreaker_",samp,"_reff_pi.csv"))
@@ -71,8 +72,8 @@ out <- out %>% mutate(scen_sim=substr(sim, 1, 2))
 
 
 #Correct names for transmission scenarios
-out$scen_sim <- case_when(out$scen_sim == "A1" ~ "CNTrW",
-                          out$scen_sim == "S4" ~ "CTrW_H")
+out$scen_sim <- case_when(out$scen_sim == "A1" ~ "CNTrW", #meaning cattle index and wild boars that did NOT transmit
+                          out$scen_sim == "S4" ~ "CTrW_H") #meaning cattle index, wild boars that transmit and high rate
 
 #Write output for supplementary
 write_csv(out, "res_contribution_pi_Se.csv")
@@ -89,7 +90,7 @@ model <- glm.nb(R_sim
 
 summary(model) #and to get p-value
 
-#Create data.frame with results
+#Create data.frame with results (S12 Table)
 all_meth <- tidy(model, exponentiate = TRUE) %>%
   mutate(OR=round(estimate, 2),
          p.value=ifelse(p.value<0.001, "<0.001", round(p.value,3)))
